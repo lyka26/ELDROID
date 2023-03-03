8a31f962451065af02d74247890a3c79ad9ca557
Lyka Marcellano <lykammarcellano@gmail.com>
2023-03-03 19:02:36 +0800

    updated threading

--- app/src/main/java/com/example/marcellano_loginregister/WelcomeActivity.java
+++ app/src/main/java/com/example/marcellano_loginregister/WelcomeActivity.java
@@ -2,67 +2,67 @@ package com.example.marcellano_loginregister;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
-
-    Button closeThread, nextThread;
-    ImageView adImage;
-    private volatile boolean closeThreadFlag = false;
-    private Handler mainHandler = new Handler();
+//  this is set as volatile so if there are changes made it will be immediately visible to all threads that access it.
+    private volatile boolean skipButtonFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        adImage = findViewById(R.id.adImageView);
-        adImage.setImageResource(R.drawable.ad1);
-        nextThread = findViewById(R.id.nextButton);
-        closeThread = findViewById(R.id.closeButton);
+//      create a new instance of the Dialog class
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.ads_layout);
 
-        nextThread.setOnClickListener(new View.OnClickListener() {
+//      close dialog on touch outside
+//      dialog.setCanceledOnTouchOutside(true);
+//      Set title
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT); // fullscreen
+        dialog.setTitle("Advertisement");
+
+//      Creates a new instance of the Handler class
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                nextThread(5);
+            public void run() {
+                dialog.show();
             }
-        });
+            // delay in milliseconds, here it's set to 5 seconds
+        }, 5000);
 
-        closeThread.setOnClickListener(new View.OnClickListener() {
+        Button skipButton = dialog.findViewById(R.id.skipButton);
+        View skipThread = findViewById(R.id.skipButton);
+
+        skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                closeThread();
+            public void onClick(View v) {
+                skipThread (5);
+                dialog.dismiss();
             }
         });
     }
 
-    public void nextThread(int seconds)
+    public void skipThread (int seconds)
     {
         for(int i =0; i< seconds; i++){
             Log.d("THREAD ACTIVITY", "Start Thread : " + i);
             try
             {
                 Thread.sleep(1000);
-            } catch (InterruptedException e)
-            {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        AdsRunnable runnable = new AdsRunnable(5);
+        AdsRunnable runnable = new AdsRunnable(10);
         new Thread(runnable).start();
-    }
-
-    public void closeThread()
-    {
-        closeThreadFlag = true;
-        adImage.setVisibility(View.GONE);
-        nextThread.setEnabled(false);
-        closeThread.setEnabled(false);
     }
 
     class AdsRunnable implements Runnable
@@ -70,28 +70,19 @@ public class WelcomeActivity extends AppCompatActivity {
         int seconds;
 
         //Non-Default Constructor
-        AdsRunnable(int seconds){
+        AdsRunnable(int seconds)
+        {
             this.seconds = seconds;
         }
 
         @Override
         public void run() {
-            for(int i =0; i< seconds; i++){
+            for(int i =0; i< seconds; i++)
+            {
+                if(skipButtonFlag){
 
-                if(closeThreadFlag)
-                {
                     return;
                 }
-
-                if(i == 2) {
-                    mainHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            adImage.setImageResource(R.drawable.ad2);
-                        }
-                    });
-                }
-
                 Log.d("THREAD ACTIVITY", "Start Thread : " + i);
 
                 try
